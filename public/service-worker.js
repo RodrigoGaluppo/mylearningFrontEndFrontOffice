b3b54d51cc848386f2fe788dd5b2f6fde914cc5a
rodrigo <rodrigogaluppo788@gmail.com>
2023-06-12 21:09:50 +0100

    take cache off

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,25 @@
-// service-worker.js
-
-// Define the cache names
-const CACHE_NAME = 'mylearningbackoffice-cache-v1';
-const DATA_CACHE_NAME = 'mylearningbackoffice-data-cache-v1';
-
-// List the files to be cached
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  // Add other files and assets that should be cached
-];
-
-// Install the service worker
 self.addEventListener('install', (event) => {
   // Perform installation steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log('Service Worker installed');
 });
 
-// Intercept fetch requests
+self.addEventListener('activate', (event) => {
+  // Perform activation steps
+  console.log('Service Worker activated');
+});
+
 self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
   // Check if the request is to an API
-  if (event.request.url.includes('/api/')) {
-    event.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(event.request)
-          .then((response) => {
-            // If the response was successful, clone it and store it in the cache
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch(() => {
-            // If the network request fails, try to retrieve the data from the cache
-            return cache.match(event.request);
-          });
-      })
-    );
+  if (request.url.includes('/api/')) {
+    // Make a network request for API requests
+    event.respondWith(fetch(request));
+  } else if (request.mode === 'navigate') {
+    // Serve a fallback HTML for navigation requests
+    event.respondWith(fetch('/offline.html'));
   } else {
-    // Serve the file from the cache if available, otherwise fetch it
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
+    // Serve other files directly without caching
+    event.respondWith(fetch(request));
   }
 });
